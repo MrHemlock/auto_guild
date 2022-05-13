b00c3da5b4a4b0f786845eebd3a665f7f826b8c2
Daniel J Brown <browndj3@gmail.com>
2022-05-13 13:10:02 -0500

    Adjusted role_parser and payload_builder
    - changed layout within role_parser
    
    - changed payload_builder to return a dictionary literal rather than
    modifying a dictionary multiple times
    
    - removed double spaces in docstring

--- auto_guild.py
+++ auto_guild.py
@@ -31,13 +31,13 @@ def channel_parser(
     or 4 if they're a channel in a category
 
     name: str - The name of the channel or category
-    id: int - Placeholder id for the channel.  This will be replaced by a unique
+    id: int - Placeholder id for the channel. This will be replaced by a unique
         snowflake on Discord's end
-    type: int - Dictates what kind of channel it is.  We only care about 3 of them:
+    type: int - Dictates what kind of channel it is. We only care about 3 of them:
         0 - Text channel
         2 - Voice channel
         4 - Category
-    parent_id: int - Used for the voice and text channels.  Represents the category
+    parent_id: int - Used for the voice and text channels. Represents the category
         id that the channel is listed under
     """
     payload = []
@@ -85,15 +85,14 @@ def role_parser(roles: list[str]) -> list[dict[str, str | int]]:
 
     """
     payload = []
-    current_id = 0
 
     payload.append(
         {
             "name": "everyone",
-            "id": current_id,
+            "id": 0,
         }
     )
-    current_id += 1
+    current_id = 1
 
     for role in roles:
         payload.append(
@@ -108,12 +107,12 @@ def role_parser(roles: list[str]) -> list[dict[str, str | int]]:
 
 
 def payload_builder(config) -> dict[str, str | list[dict[str, str | int]]]:
-    payload = {}
-    payload["name"] = config["name"]
-    payload["channels"] = channel_parser(config["categories"])
-    payload["roles"] = role_parser(config["roles"])
-    payload["system_channel_id"] = 1
-    return payload
+    return {
+        "name": config["name"],
+        "channels": channel_parser(config["categories"]),
+        "roles": role_parser(config["roles"]),
+        "system_channel_id": 1,
+    }
 
 
 def create_guild(session, payload):
