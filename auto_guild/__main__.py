f863c2b9c1ebab32b888c6080173e60577d5344b
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-05-26 20:45:53 -0500

    Don't open browser if answer is empty string
    startswith returns True if the prefix is empty.
    Because of this, if the user presses enter to select the default "No", the browser is opened regardless.

--- auto_guild/__main__.py
+++ auto_guild/__main__.py
@@ -275,7 +275,7 @@ def run() -> None:
         invite_url = get_invite(session_, invite_channel_id)
         print(invite_url)
         answer = input("Would you like to open the invite link in a browser? y/N: ")
-        if "yes".startswith(answer.lower()):
+        if len(answer) > 0 and "yes".startswith(answer.lower()):
             webbrowser.open(invite_url)
 
         input("Press enter after you have joined the server")
