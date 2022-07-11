975e1e40f06470d9cfa0440149d2496fdcd18519
Daniel Brown <browndj3@gmail.com>
2022-07-11 13:57:57 -0500

    Webhook (#16)
    * Adding pydis_dev.yml file for testing elsewhere.
    
    * Added webhook support
    
    * Lazy commit message.  More stuff added, specifically for logging and testing
    
    * Added webhook support
    
    - Changed news channels back to text, as news is not considered a valid
    channel type during guild creation
    - Removed type hints for now to later attempt to make them more coherent
    - System channel ID temporarily hard coded to be 8. This is temporary
    until I can make it dynamic.
    
    * Corrected system_channel_id implementation
    Updated lock file

--- auto_guild/__main__.py
+++ auto_guild/__main__.py
@@ -11,27 +11,30 @@ from dotenv import load_dotenv
 from requests import Session
 from yaml import Loader, dump, load
 
-BASE_URL = r"https://discord.com/api/v10"
 
-PAYLOAD = dict[str, str | int | list[dict[str, str | int]]]
-PARSED_CHANNELS = list[dict[str, str | int]]
+BASE_URL = r"https://discord.com/api/v10"
+CHANNEL_TYPES = {
+    "text": 0,
+    "voice": 2,
+    "category": 4,
+}
 
 
 class InvalidChannelType(Exception):
     """Raised when the channel type is invalid."""
 
-    def __init__(self, channel_type: str) -> None:
+    def __init__(self, channel_type):
         self.channel_type: str = channel_type
         self.message: str = (
             "{} is not a valid channel type.  Please consult the readme."
         )
         super().__init__(self.message)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.message.format(self.channel_type)
 
 
-def channel_parser(channel_mapping: dict[str, list[dict[str, str]]]) -> PARSED_CHANNELS:
+def channel_parser(channel_mapping):
     """Builds a list of channel objects to pass to the API
 
     Channels must consist of either 3 things if they're a category
@@ -50,41 +53,46 @@ def channel_parser(channel_mapping: dict[str, list[dict[str, str]]]) -> PARSED_C
 
     payload = []
     current_id = 0
+    system_channel_id = None
 
-    for category, channels in channel_mapping.items():
+    for category in channel_mapping:
         parent_id = current_id
+        channels = {}
+
+        if isinstance(category, dict):
+            category_name, channels = next(iter(category.items()))
+        else:
+            category_name = category
 
         payload.append(
             {
-                "name": category,
+                "name": category_name,
                 "id": parent_id,
                 "type": 4,
             }
         )
-        current_id += 1
-        for channel in channels:
-            ((name, type_),) = channel.items()
-            if type_ == "voice":
-                type_id = 2
-            elif type_ == "text":
-                type_id = 0
-            else:
-                raise InvalidChannelType(type_)
 
+        current_id += 1
+
+        for channel in channels:
+            channel_name, type_ = next(iter(channel.items()))
+            if (type_id := CHANNEL_TYPES.get(type_)) is None:
+                raise InvalidChannelType(type_)
             payload.append(
                 {
-                    "name": name,
+                    "name": channel_name,
                     "id": current_id,
                     "type": type_id,
                     "parent_id": parent_id,
                 }
             )
+            if system_channel_id is None:
+                system_channel_id = current_id
             current_id += 1
+    return payload, system_channel_id
 
-    return payload
 
-
-def role_parser(roles: list[str]) -> list:
+def role_parser(roles):
     """Returns a list of role objects to pass to the API
 
     Role objects submitted to the Create Guild endpoint only require
@@ -109,44 +117,72 @@ def role_parser(roles: list[str]) -> list:
             }
         )
         current_id += 1
-
     return payload
 
 
 def payload_builder(
     config,
+    *,
     name=None,
-) -> PAYLOAD:
+    system_channel_id=1,
+):
     """Builds the complete payload to pass to the API"""
-    payload: PAYLOAD = {"system_channel_id": 1}
+    payload = {}
     if categories := config.get("categories"):
-        payload.update(channels=channel_parser(categories))
+        channels_, system_channel_id = channel_parser(categories)
+        payload.update(channels=channels_)
+    payload.update(system_channel_id=system_channel_id)
+
     if roles := config.get("roles"):
         payload.update(roles=role_parser(roles))
     if name_ := name or config.get("name"):
         payload.update(name=name_)
-
     return payload
 
 
-def create_guild(session: Session, payload: PAYLOAD) -> dict:
+def create_guild(session, payload):
     """Creates a guild using the API"""
     response = session.post(
         f"{BASE_URL}/guilds",
         json=payload,
     )
+    response.raise_for_status()
+
     return response.json()
 
 
-def get_channels(session: Session, guild_id: int) -> list[dict]:
+def get_channels(session, guild_id):
     """Gets all channels in a guild"""
     response = session.get(f"{BASE_URL}/guilds/{guild_id}/channels")
     return response.json()
 
 
+def create_webhooks(
+    session,
+    webhook_channels,
+    channels_,
+):
+    webhooks = []
+
+    for channel in channels_:
+        if channel.get("type") == 4:
+            continue
+        if (channel_name := channel.get("name")) not in webhook_channels:
+            continue
+        channel_id = channel.get("id")
+        response = session.post(
+            f"{BASE_URL}/channels/{channel_id}/webhooks",
+            json={"name": channel_name},
+        )
+        webhooks.append(response.json())
+
+    return webhooks
+
+
 def compile_finished_guild(
-    channels: list[dict],
-    roles: list[dict],
+    channels,
+    roles,
+    webhooks,
 ):
     """Compiles the guild details into the format that the bot expects"""
     finished_guild = {"categories": {}, "roles": []}
@@ -167,10 +203,17 @@ def compile_finished_guild(
         role_list.append({name: id_})
     finished_guild["roles"] = role_list
 
+    webhook_list = []
+    for webhook in webhooks:
+        name = webhook["name"]
+        id_ = webhook["id"]
+        webhook_list.append({name: id_})
+    finished_guild["webhooks"] = webhook_list
+
     return finished_guild
 
 
-def get_invite(session: Session, channel_id: str) -> str:
+def get_invite(session, channel_id):
     """Creates an invite for the given channel and returns the invite URL"""
     response = session.post(
         f"{BASE_URL}/channels/{channel_id}/invites",
@@ -180,7 +223,7 @@ def get_invite(session: Session, channel_id: str) -> str:
     return f"https://discord.gg/{invite_id}"
 
 
-def transfer_ownership(session: Session, user_id: str, guild_id: str) -> None:
+def transfer_ownership(session, user_id, guild_id):
     """Transfers ownership of a guild to a user"""
     session.patch(
         f"{BASE_URL}/guilds/{guild_id}",
@@ -188,7 +231,7 @@ def transfer_ownership(session: Session, user_id: str, guild_id: str) -> None:
     )
 
 
-def run() -> None:
+def run():
     """Runs the script"""
     description = (
         "Create a Discord guild with the specified configuration. "
@@ -230,21 +273,22 @@ def run() -> None:
     args = parser.parse_args()
     load_dotenv()
 
-    USER_ID: str | None = args.user_id or getenv("USER_ID")
+    USER_ID = args.user_id or getenv("USER_ID")
     if not USER_ID:
         raise ValueError("USER_ID not found")
-    BOT_TOKEN: str | None = args.bot_token or getenv("BOT_TOKEN")
+    BOT_TOKEN = args.bot_token or getenv("BOT_TOKEN")
     if not BOT_TOKEN:
         raise ValueError("BOT_TOKEN not found")
     template_path = args.structure
 
-    SERVER_NAME: str | None = args.server_name
+    SERVER_NAME = args.server_name
+    webhook_list = None
     if SERVER_NAME:
         payload_ = payload_builder({"name": SERVER_NAME})
     else:
         with open(template_path) as file:
             dumped = load(file, Loader=Loader)
-
+            webhook_list = dumped.get("webhooks")
         payload_ = payload_builder(dumped)
 
     initialized = Session()
@@ -258,13 +302,18 @@ def run() -> None:
 
     with initialized as session_:
         guild_response = create_guild(session_, payload_)
+        # print(guild_response)
         guild_id_ = guild_response["id"]
         guild_roles = guild_response["roles"]
         invite_channel_id = guild_response["system_channel_id"]
 
         channels_ = get_channels(session_, guild_id_)
 
-        finished_guild_ = compile_finished_guild(channels_, guild_roles)
+        webhook_objects = []
+        if webhook_list is not None:
+            webhook_objects = create_webhooks(session_, webhook_list, channels_)
+
+        finished_guild_ = compile_finished_guild(channels_, guild_roles, webhook_objects)
 
         output_path = Path("guild_layout.yaml")
 
@@ -278,7 +327,7 @@ def run() -> None:
         if len(answer) > 0 and "yes".startswith(answer.lower()):
             webbrowser.open(invite_url)
 
-        input("Press enter after you have joined the server")
+        input("Press enter after you have joined the server ")
         transfer_ownership(session_, USER_ID, guild_id_)
         print("Ownership transferred")
 
